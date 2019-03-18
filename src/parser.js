0550898685dd7d1673fa583af7bb3749e5f9ea28
Hugo Alliaume <kocal@live.fr>
2019-03-18 09:10:39 +0100

    feat: implement Parser, the first test is correctly executed! :)

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,14 @@
+const path = require("path");
+const { TwingEnvironment, TwingSource } = require("twing");
+
+const twing = new TwingEnvironment();
+
 function parse(text, parsers, opts) {
-  console.log({ text, parsers, opts });
+  const source = new TwingSource(text, path.basename(opts.filepath), opts.filepath);
+  const tokens = twing.tokenize(source);
+  const nodes = twing.parse(tokens);
+
+  return nodes.getNode('body');
 }
 
 module.exports = parse;
