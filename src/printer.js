4ade6095b3cd112aa731359edc8bf49789f03f97
Hugo Alliaume <kocal@live.fr>
2019-03-21 21:07:22 +0100

    feat: printer implementation + support for `{% set %}` (#3)
    Also we should wait for https://github.com/ericmorand/twing/pull/326 before merging this one (or we will open a new PR), because actually `{%-` are converted to `{%` 😔.

--- src/printer.js
+++ src/printer.js
@@ -1,7 +1,114 @@
-function genericPrint(path, options, print) {
-  console.log({ path, options, print });
+const { TwingNode, TwingNodeType } = require("twing");
+const { join, hardline, concat } = require("prettier").doc.builders;
+const util = require("./_util-from-prettier");
 
-  return 'printer output';
+// The root, in fact that's not really a node, but it contains references to blocks, macros, ...
+let nodeModule;
+
+function printString(rawContent, options) {
+  const double = {
+    quote: "\"",
+    regex: /"/g
+  };
+  const single = {
+    quote: "'",
+    regex: /'/g
+  };
+
+  const preferred = options.singleQuote ? single : double;
+  const alternate = preferred === single ? double : single;
+
+  let shouldUseAlternateQuote = false;
+
+  // If `rawContent` contains at least one of the quote preferred for enclosing
+  // the string, we might want to enclose with the alternate quote instead, to
+  // minimize the number of escaped quotes.
+  // Also check for the alternate quote, to determine if we're allowed to swap
+  // the quotes on a DirectiveLiteral.
+  if (rawContent.includes(preferred.quote) || rawContent.includes(alternate.quote)) {
+    const numPreferredQuotes = (rawContent.match(preferred.regex) || []).length;
+    const numAlternateQuotes = (rawContent.match(alternate.regex) || []).length;
+
+    shouldUseAlternateQuote = numPreferredQuotes > numAlternateQuotes;
+  }
+
+  const quote = shouldUseAlternateQuote ? alternate.quote : preferred.quote;
+
+  return util.makeString(rawContent, quote);
+}
+
+/**
+ * @param {FastPath|TwingNode} path
+ * @param {Object} options
+ * @param {Function} print
+ * @return {string}
+ */
+function genericPrint(path, options, print) {
+  /** @type TwingNode */
+  let node = path instanceof TwingNode ? path : path.getValue();
+
+  if (!node) {
+    return "";
+  }
+
+  if (node.getType() === TwingNodeType.MODULE) {
+    nodeModule = node;
+    node = nodeModule.getNode("body");
+  }
+
+  switch (node.getType()) {
+    case null:
+    case TwingNodeType.BODY: {
+      return concat(
+        Array.from(node.getNodes().values()).map(n => genericPrint(n, options, print))
+      );
+    }
+    case TwingNodeType.EXPRESSION_CONSTANT: {
+      const value = node.getAttribute("value");
+
+      if (typeof value === "string") {
+        return printString(value, options);
+      }
+
+      return String(value);
+    }
+    case TwingNodeType.SET: {
+      const names = Array.from(
+        node
+          .getNode("names")
+          .getNodes()
+          .values()
+      ).map(n => n.getAttribute("name"));
+
+      const values = Array.from(
+        node
+          .getNode("values")
+          .getNodes()
+          .values()
+      ).map(n => genericPrint(n, options, print));
+
+      return join(" ", [
+        "{%",
+        node.getNodeTag(),
+        join(", ", names),
+        "=",
+        join(", ", values),
+        "%}",
+        hardline
+      ]);
+    }
+    case TwingNodeType.EXPRESSION_ARRAY: {
+      const values = Array.from(node.getNodes().values())
+        .filter((n, i) => i % 2 !== 0) // filter array keys
+        .map(n => genericPrint(n, options, print));
+
+      return concat(["[", join(", ", values), "]"]);
+    }
+    default:
+      return "ok";
+  }
+
+  return "";
 }
 
 module.exports = genericPrint;
