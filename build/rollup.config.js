b04a7fb9bd55244d04ea442903c910d7d3985684
Hugo Alliaume <kocal@live.fr>
2019-03-27 22:45:57 +0100

    Build standalone.js
    Thanks https://github.com/prettier/plugin-php/pull/950 <3

---
+++ build/rollup.config.js
@@ -0,0 +1,55 @@
+import { resolve } from "path";
+
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from 'rollup-plugin-json';
+import replace from "rollup-plugin-replace";
+import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+const SRC_DIR = resolve(__dirname, "..", "src");
+const BUILD_DIR = resolve(__dirname, "..", "build");
+
+export default {
+  input: resolve(SRC_DIR, "index.js"),
+  output: {
+    file: "standalone.js",
+    format: "umd",
+    name: "prettierPlugins.twig",
+    exports: "named",
+    globals: {
+      prettier: "prettier"
+    },
+    paths: {
+      prettier: "prettier/standalone"
+    }
+  },
+  external: ["prettier"],
+  plugins: [
+    builtins(),
+    globals(),
+    nodeResolve(),
+    commonjs(),
+    json(),
+    replace({
+      "process.arch": JSON.stringify("x32")
+    }),
+    babel({
+      babelrc: false,
+      plugins: [],
+      compact: false,
+      presets: [
+        [
+          require.resolve("@babel/preset-env"),
+          {
+            targets: { browsers: [">0.25%", "not ie 11", "not op_mini all"] },
+            modules: false
+          }
+        ]
+      ]
+    }),
+    terser()
+  ]
+};
