dc11c4f3640a12adcb591281f28eed80bd3a6f9c
Hugo Alliaume <kocal@live.fr>
2019-03-28 14:26:13 +0100

    playground: add bootstrap & bootstrap-vue

--- playground/src/main.js
+++ playground/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
