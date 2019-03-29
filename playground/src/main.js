d952c583e088715eb4d45d426bb3e8bcc60bc0ab
Hugo Alliaume <kocal@live.fr>
2019-03-29 07:19:32 +0100

    playground: add Vuex

--- playground/src/main.js
+++ playground/src/main.js
@@ -10,11 +10,13 @@ import "codemirror/mode/twig/twig.js";
 import "codemirror/addon/display/rulers.js";
 
 import App from "./App.vue";
+import store from './store'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VueCodemirror);
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount("#app");
